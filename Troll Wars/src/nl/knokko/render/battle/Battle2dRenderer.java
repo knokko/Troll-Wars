1fb9335502e0cf1bfc31bb02f8cb76786b14ec20
knokko <knokogator@hotmail.com>
2018-09-14 22:02:49 +0200

    Add QUAD model to Battle2dRenderer

--- Troll Wars/src/nl/knokko/render/battle/Battle2dRenderer.java
+++ Troll Wars/src/nl/knokko/render/battle/Battle2dRenderer.java
@@ -3,6 +3,7 @@ package nl.knokko.render.battle;
 import nl.knokko.battle.creature.BattleCreature;
 import nl.knokko.battle.render.properties.BattleRenderProperties;
 import nl.knokko.model.type.AbstractModel;
+import nl.knokko.model.type.GuiModel;
 import nl.knokko.view.camera.Camera;
 
 import org.lwjgl.opengl.GL11;
@@ -13,13 +14,16 @@ import org.lwjgl.util.vector.Vector2f;
 import org.lwjgl.util.vector.Vector3f;
 
 import static nl.knokko.shaders.battle.ResourceShader.RESOURCE_SHADER;
+import nl.knokko.util.resources.Resources;
 
 public class Battle2dRenderer {
+    
+        public static final GuiModel QUAD = Resources.loadGuiModel(new float[]{-1,1, -1,-1, 1,1, 1,-1});
 	
 	public void startResourceShader(Matrix4f viewMatrix){
 		RESOURCE_SHADER.start();
 		RESOURCE_SHADER.loadViewMatrix(viewMatrix);
-		prepareModel(GuiRenderer.QUAD);
+		prepareModel(QUAD);
 	}
 	
 	public void renderResources(Camera camera, BattleCreature creature){
@@ -61,7 +65,7 @@ public class Battle2dRenderer {
 		RESOURCE_SHADER.loadProgress((float) current / max);
 		RESOURCE_SHADER.loadWorldPosition(new Vector3f(midX, midY, midZ));
 		RESOURCE_SHADER.loadScale(new Vector2f(scaleX, scaleY));
-		GL11.glDrawArrays(GL11.GL_TRIANGLE_STRIP, 0, GuiRenderer.QUAD.getVertexCount());
+		GL11.glDrawArrays(GL11.GL_TRIANGLE_STRIP, 0, QUAD.getVertexCount());
 	}
 	
 	public void stopResourceShader(){
