be0b0468ac0ce29fad12592dce2aab761c64434c
knokko <knokogator@hotmail.com>
2018-10-06 19:05:02 +0200

    Hopefully finish GuiInventory

--- Troll Wars/src/nl/knokko/gui/menu/game/GuiInventory.java
+++ Troll Wars/src/nl/knokko/gui/menu/game/GuiInventory.java
@@ -23,38 +23,36 @@
  */
 package nl.knokko.gui.menu.game;
 
-import org.lwjgl.util.vector.Vector2f;
+import java.awt.Color;
 
 import nl.knokko.gamestate.StateGameMenu;
 import nl.knokko.gui.button.ButtonCloseMenu;
 import nl.knokko.gui.button.ButtonLink;
+import nl.knokko.gui.color.GuiColor;
+import nl.knokko.gui.color.SimpleGuiColor;
+import nl.knokko.gui.component.GuiComponent;
 import nl.knokko.gui.component.WrapperComponent;
 import nl.knokko.gui.component.inventory.ComponentInventory;
-import nl.knokko.gui.component.inventory.ComponentInventory.ClickAction;
 import nl.knokko.gui.component.menu.GuiMenu;
-import nl.knokko.gui.render.GuiRenderer;
+import nl.knokko.gui.component.text.ConditionalTextButton;
+import nl.knokko.gui.component.text.TextButton;
+import nl.knokko.gui.util.TextBuilder.Properties;
+import nl.knokko.inventory.InventoryType;
 import nl.knokko.inventory.InventoryTypeBase;
 import nl.knokko.items.Item;
 import nl.knokko.main.Game;
-import nl.knokko.texture.SizedTexture;
-import nl.knokko.util.color.Color;
-import nl.knokko.util.resources.Resources;
 
 public class GuiInventory extends GuiMenu {
 	
-	private static final Color BUTTON_COLOR = new Color(100, 150, 0);
-	private static final Color BORDER_COLOR = new Color(40, 50, 0);
-	private static final Color BACKGROUND_COLOR = new Color(150, 100, 0);
+	private static final Properties BUTTON_PROPS = Properties.createButton(new Color(100, 150, 0), new Color(40, 50, 0));
+	private static final Properties HOVER_PROPS = Properties.createButton(new Color(125, 200, 0), new Color(50, 65, 0));
+	
+	private static final GuiColor BACKGROUND_COLOR = new SimpleGuiColor(150, 100, 0);
 	
 	private final StateGameMenu state;
-	
-	private SizedTexture materialsTexture;
-	private SizedTexture equipmentTexture;
-	private SizedTexture consumablesTexture;
     
-    private InventoryTypeBase selectedType;
-	
-	private boolean hasTextures;
+    private InventoryType selectedType;
+    private InventoryTypeBase selectedBaseType;
 
 	public GuiInventory(StateGameMenu menu) {
 		state = menu;
@@ -62,79 +60,99 @@ public class GuiInventory extends GuiMenu {
 	
 	@Override
 	protected void addComponents(){
-        
-        //TODO maybe, I should do a real rework
-        
-		/*
-		addButton(new InventoryTypeButton(new Vector2f(-0.65f, 0.7f), null));
-		addButton(new InventoryTypeButton(new Vector2f(-0.65f, 0.45f), InventoryTypeBase.MATERIAL));
-		addButton(new InventoryTypeButton(new Vector2f(-0.65f, 0.2f), InventoryTypeBase.EQUIPMENT));
-		addButton(new InventoryTypeButton(new Vector2f(-0.65f, -0.05f), InventoryTypeBase.CONSUMABLE));
-		*/
-        addComponent(new ComponentInventory(Game.getPlayerInventory().getItems(), (Item item) -> {}));
-		addButton(new ButtonLink(new Vector2f(-0.65f, -0.6f), new Vector2f(0.3f, 0.1f), BUTTON_COLOR, BORDER_COLOR, Color.BLACK, "Back to menu", state.getGameMenu(), state));
-		addButton(new ButtonCloseMenu(new Vector2f(-0.65f, -0.85f), new Vector2f(0.3f, 0.1f), BUTTON_COLOR, BORDER_COLOR));
+		addComponent(new ButtonLink("Back to menu", state, state.getGameMenu(), BUTTON_PROPS, HOVER_PROPS), 0.05f, 0.7f, 0.2f, 0.8f);
+		addComponent(new ButtonCloseMenu(BUTTON_PROPS, HOVER_PROPS), 0.05f, 0.5f, 0.2f, 0.6f);
+		
+		// Base type buttons
+		addBaseTypeButton("All items", null, 0.7f);
+		addBaseTypeButton(InventoryTypeBase.MATERIAL, 0.55f);
+		addBaseTypeButton(InventoryTypeBase.EQUIPMENT, 0.4f);
+		addBaseTypeButton(InventoryTypeBase.CONSUMABLE, 0.25f);
+		
+		// Materials
+		addTypeButton(InventoryType.MONEY, 0.7f);
+		addTypeButton(InventoryType.METAL, 0.575f);
+		addTypeButton(InventoryType.GEM, 0.45f);
+		addTypeButton(InventoryType.ORGANIC, 0.325f);
+		
+		// Equipment
+		addTypeButton(InventoryType.WEAPON, 0.85f);
+		addTypeButton(InventoryType.HELMET, 0.725f);
+		addTypeButton(InventoryType.CHESTPLATE, 0.6f);
+		addTypeButton(InventoryType.PANTS, 0.475f);
+		addTypeButton(InventoryType.BOOTS, 0.35f);
+		addTypeButton(InventoryType.GLOBE, 0.225f);
+		
+		// Consumables
+		addTypeButton(InventoryType.DRINK, 0.7f);
+		addTypeButton(InventoryType.FOOD, 0.5f);
+		
+		// Inventory wrapper components
+		addWrapper(new InventoryBaseTypeWrapper(null));
+		InventoryTypeBase[] baseValues = InventoryTypeBase.values();
+		for(InventoryTypeBase base : baseValues)
+			addWrapper(new InventoryBaseTypeWrapper(base));
+		
+		InventoryType[] values = InventoryType.values();
+		for(InventoryType type : values)
+			addWrapper(new InventoryTypeWrapper(type));
+	}
+	
+	private void addWrapper(GuiComponent component) {
+		addComponent(component, 0.75f, 0.05f, 0.95f, 0.95f);
+	}
+	
+	private void addTypeButton(InventoryType type, float minY) {
+		addComponent(new ConditionalTextButton(type.toString(), BUTTON_PROPS, HOVER_PROPS, () -> {
+			selectedType = type;
+		}, () -> {
+			return selectedBaseType == type.getParent();
+		}), 0.5f, minY, 0.7f, minY + 0.1f);
+	}
+	
+	private void addBaseTypeButton(String text, InventoryTypeBase type, float minY) {
+		addComponent(new TextButton(text, BUTTON_PROPS, HOVER_PROPS, () -> {
+			selectedBaseType = type;
+			selectedType = null;
+		}), 0.25f, minY, 0.45f, minY + 0.1f);
+	}
+	
+	private void addBaseTypeButton(InventoryTypeBase type, float minY) {
+		addBaseTypeButton(type.toString(), type, minY);
 	}
 	
 	@Override
-	public void render(GuiRenderer renderer){
-		renderer.render(textures, buttons, this, false);
-		if(!hasTextures){
-			hasTextures = true;
-			materialsTexture = Resources.createInventoryTexture(Game.getPlayerInventory(), Game.getPlayerInventory().getItems(InventoryTypeBase.MATERIAL));
-			equipmentTexture = Resources.createInventoryTexture(Game.getPlayerInventory(), Game.getPlayerInventory().getItems(InventoryTypeBase.EQUIPMENT));
-			consumablesTexture = Resources.createInventoryTexture(Game.getPlayerInventory(), Game.getPlayerInventory().getItems(InventoryTypeBase.CONSUMABLE));
-		}
-		if(materialsTexture != null)
-			renderer.renderTextures(new Vector2f(-0.1f, 1 - materialsTexture.getRelativeHeight()), new Vector2f(0.2f, materialsTexture.getRelativeHeight()), materialsTexture);
-		if(equipmentTexture != null)
-			renderer.renderTextures(new Vector2f(0.35f, 1 - equipmentTexture.getRelativeHeight()), new Vector2f(0.2f, equipmentTexture.getRelativeHeight()), equipmentTexture);
-		if(consumablesTexture != null)
-			renderer.renderTextures(new Vector2f(0.8f, 1 - consumablesTexture.getRelativeHeight()), new Vector2f(0.2f, consumablesTexture.getRelativeHeight()), consumablesTexture);
-	}
-	
-	@Override
-	public Color getBackgroundColor(){
+	public GuiColor getBackgroundColor(){
 		return BACKGROUND_COLOR;
 	}
-	
-	/*
-	private class InventoryTypeButton extends ButtonText {
-		
-		private SizedTexture texture;
-		private InventoryTypeBase type;
-
-		public InventoryTypeButton(Vector2f centre, InventoryTypeBase type) {
-			super(centre, new Vector2f(0.3f, 0.1f), BUTTON_COLOR, BORDER_COLOR, Color.BLACK, type != null ? type.name().toLowerCase() : "all items");
-			this.type = type;
-		}
-
-		@Override
-		public void leftClick(float x, float y) {
-			if(texture == null)
-				texture = Resources.createInventoryTexture(Game.getPlayerInventory(), type != null ? Game.getPlayerInventory().getItems(type) : Game.getPlayerInventory().getItems());
-			currentTexture = texture;
-		}
-		
-	}
-	*/
     
-    private class TypeWrapper extends WrapperComponent<ComponentInventory> {
+    private class InventoryTypeWrapper extends WrapperComponent<ComponentInventory> {
         
-        private final InventoryTypeBase type;
+        private final InventoryType type;
         
-        public TypeWrapper(InventoryTypeBase type) {
+        public InventoryTypeWrapper(InventoryType type) {
             super(new ComponentInventory(Game.getPlayerInventory().getItems(type), (Item item) -> {}));
             this.type = type;
         }
         
         @Override
         public boolean isActive(){
-            return selectedType == null || selectedType == type;
+            return selectedType == type;
+        }
+    }
+    
+    private class InventoryBaseTypeWrapper extends WrapperComponent<ComponentInventory> {
+        
+        private final InventoryTypeBase type;
+        
+        public InventoryBaseTypeWrapper(InventoryTypeBase type) {
+            super(new ComponentInventory(type != null ? Game.getPlayerInventory().getItems(type) : Game.getPlayerInventory().getItems(), (Item item) -> {}));
+            this.type = type;
         }
         
-        public void refresh(){
-            component.refresh(Game.getPlayerInventory().getItems(type));
+        @Override
+        public boolean isActive(){
+            return selectedBaseType == type;
         }
     }
 }
