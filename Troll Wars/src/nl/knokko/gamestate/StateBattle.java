3066221f73b7e2c433d61a577820a98d9a03a205
knokko <knokogator@hotmail.com>
2019-06-08 17:16:50 +0200

    Remove debug messages

--- Troll Wars/src/nl/knokko/gamestate/StateBattle.java
+++ Troll Wars/src/nl/knokko/gamestate/StateBattle.java
@@ -64,25 +64,21 @@ public class StateBattle implements GameState {
 		if(battle == null)
 			throw new IllegalStateException("There is no battle!");
 		Game.getWindow().setRenderContinuously(true);
-		System.out.println("continous true (battle)");
 	}
 
 	@Override
 	public void close() {
 		Game.getWindow().setRenderContinuously(false);
-		System.out.println("continous false (battle)");
 	}
 
 	@Override
 	public void enable() {
 		Game.getWindow().setRenderContinuously(true);
-		System.out.println("continous true (battle)");
 	}
 
 	@Override
 	public void disable() {
 		Game.getWindow().setRenderContinuously(false);
-		System.out.println("continous false (battle)");
 	}
 
 	@Override
